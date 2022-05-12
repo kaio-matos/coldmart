f78edd6be4e2abc182e903f2efcf1301ed914282
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-11 21:25:04 -0300

    globals.css - Adding global css and default link style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import styles from './header.module.css'
+import Link from "next/link";
+import styles from "./header.module.css";
 
 function Header() {
     return (
@@ -9,18 +9,38 @@ function Header() {
             </div>
             <div className={styles.header__menu}>
                 <ul>
-                    <li><Link href="/solutions"><a>Soluções</a></Link></li>
-                    <li><Link href="/card"><a>Cartão Coldmart</a></Link></li>
-                    <li><Link href="/affiliates"><a>Afiliados</a></Link></li>
-                    <li><Link href="/buyclass"><a>Comprar um curso</a></Link></li>
+                    <li>
+                        <Link href="/solutions">
+                            <a className="link">Soluções</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/card">
+                            <a className="link">Cartão Coldmart</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/affiliates">
+                            <a className="link">Afiliados</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/buyclass">
+                            <a className="link">Comprar um curso</a>
+                        </Link>
+                    </li>
                 </ul>
             </div>
             <div className={styles.header__buttons}>
-                <Link href="/login"><a>Entrar</a></Link>
-                <Link href="/register"><a>Cadastre-se</a></Link>
+                <Link href="/login">
+                    <a>Entrar</a>
+                </Link>
+                <Link href="/register">
+                    <a>Cadastre-se</a>
+                </Link>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
