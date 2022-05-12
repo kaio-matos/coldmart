4c2c17f1e60f8b5be2442c08f76459c8d27d6757
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-12 20:32:08 -0300

    components:Header - Desktop header styles and components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,43 +2,51 @@ import Link from "next/link";
 import Button from "../Buttons/Button";
 import styles from "./header.module.css";
 
+type MenuProps = {
+    itens: { href: string; name: string }[];
+};
+
+function Menu({ itens }: MenuProps) {
+    return (
+        <ul className={styles.header__container__menu__ul}>
+            {itens.map(({ href, name }) => (
+                <li key={name}>
+                    <Link href={href}>
+                        <a className="link">{name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
 function Header() {
+    const menu_itens = [
+        { href: "/solutions", name: "Soluções" },
+        { href: "/card", name: "Cartão Coldmart" },
+        { href: "/affiliates", name: "Afiliados" },
+        { href: "/academy", name: "Academy" },
+        { href: "/buyclass", name: "Comprar um curso" },
+    ];
+
     return (
         <header className={styles.header}>
-            <div className={styles.header__logo}>
-                <h1>ColdMart</h1>
-            </div>
-            <div className={styles.header__menu}>
-                <ul>
-                    <li>
-                        <Link href="/solutions">
-                            <a className="link">Soluções</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/card">
-                            <a className="link">Cartão Coldmart</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/affiliates">
-                            <a className="link">Afiliados</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/buyclass">
-                            <a className="link">Comprar um curso</a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div className={styles.header__buttons}>
-                <Link href="/login">
-                    <Button type="light">Entrar</Button>
-                </Link>
-                <Link href="/register">
-                    <Button>Cadastre-se</Button>
-                </Link>
+            <div className={styles.header__container}>
+                <div className={styles.header__container__logo}>
+                    <h1>ColdMart</h1>
+                </div>
+                <nav className={styles.header__container__menu}>
+                    <Menu itens={menu_itens} />
+                </nav>
+                <div></div>
+                <div className={styles.header__container__buttons}>
+                    <Link href="/login">
+                        <Button type="light">Entrar</Button>
+                    </Link>
+                    <Link href="/register">
+                        <Button>Cadastre-se</Button>
+                    </Link>
+                </div>
             </div>
         </header>
     );
