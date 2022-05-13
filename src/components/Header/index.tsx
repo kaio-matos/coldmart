aaa64b8d1cd907ca86adb2dc9a20d83a740ee008
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-12 22:31:11 -0300

    components:Header - Adding mobile header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Link from "next/link";
+import { GrFormClose } from "react-icons/gr";
+import { AiOutlineMenu } from "react-icons/ai";
 import Button from "../Buttons/Button";
 import styles from "./header.module.css";
 
-type MenuProps = {
+interface MenuProps {
     itens: { href: string; name: string }[];
-};
+}
 
 function Menu({ itens }: MenuProps) {
     return (
@@ -20,7 +23,77 @@ function Menu({ itens }: MenuProps) {
     );
 }
 
+interface MobileProps extends MenuProps {
+    isActive: boolean;
+    setIsActive: Dispatch<SetStateAction<boolean>>;
+}
+
+function Mobile({ itens, isActive, setIsActive }: MobileProps) {
+    return (
+        <div
+            className={`${styles.header__mobile} ${
+                isActive && styles.header__mobile_active
+            }`}
+        >
+            <div className={styles.header__mobile__title}>
+                <span>Menu</span>
+                <button
+                    className={styles.header__mobile__title_button}
+                    onClick={() => setIsActive(!isActive)}
+                >
+                    <GrFormClose fontSize={"2rem"} color="#545b64" />
+                </button>
+            </div>
+            <nav className={styles.header__mobile__menu}>
+                <Menu itens={itens} />
+            </nav>
+            <div className={styles.header__mobile__buttons}>
+                <Link href="/buys">
+                    <Button
+                        rounded
+                        style={{
+                            fontSize: "1.1rem",
+                            width: "100%",
+                        }}
+                        type="light-dark"
+                    >
+                        Acessar minhas compras
+                    </Button>
+                </Link>
+                <Link href="/manage">
+                    <Button
+                        rounded
+                        style={{
+                            fontSize: "1.1rem",
+                            width: "100%",
+                        }}
+                        type="light-dark"
+                    >
+                        Gerenciar meus negócios
+                    </Button>
+                </Link>
+                <Link href="/register">
+                    <Button
+                        rounded
+                        style={{ fontSize: "1.1rem", width: "100%" }}
+                    >
+                        Cadastre-se
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
 function Header() {
+    const [isDesktop, setIsDesktop] = useState(true);
+    const [mobileActive, setMobileActive] = useState(true);
+
+    useEffect(() => {
+        if (window.innerWidth < 1280) setIsDesktop(false);
+        else setIsDesktop(true);
+    });
+
     const menu_itens = [
         { href: "/solutions", name: "Soluções" },
         { href: "/card", name: "Cartão Coldmart" },
@@ -36,18 +109,47 @@ function Header() {
                     <h1>ColdMart</h1>
                 </div>
                 <nav className={styles.header__container__menu}>
-                    <Menu itens={menu_itens} />
+                    {isDesktop && <Menu itens={menu_itens} />}
                 </nav>
                 <div></div>
                 <div className={styles.header__container__buttons}>
-                    <Link href="/login">
-                        <Button type="light">Entrar</Button>
-                    </Link>
-                    <Link href="/register">
-                        <Button>Cadastre-se</Button>
-                    </Link>
+                    {isDesktop ? (
+                        <>
+                            <Link href="/login">
+                                <Button type="light-primary">Entrar</Button>
+                            </Link>
+                            <Link href="/register">
+                                <Button>Cadastre-se</Button>
+                            </Link>
+                        </>
+                    ) : (
+                        <button
+                            onClick={() => setMobileActive(!mobileActive)}
+                            className={
+                                styles.header__container__buttons__mobile_menu
+                            }
+                        >
+                            <AiOutlineMenu fontSize={"1.5rem"} />
+                        </button>
+                    )}
                 </div>
             </div>
+            {!isDesktop && (
+                <>
+                    <Mobile
+                        itens={menu_itens}
+                        isActive={mobileActive}
+                        setIsActive={setMobileActive}
+                    />
+
+                    <div
+                        className={`${styles.header__mobile__background} ${
+                            mobileActive &&
+                            styles.header__mobile__background_active
+                        }`}
+                    ></div>
+                </>
+            )}
         </header>
     );
 }
