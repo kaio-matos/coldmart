0eede61d0a108662cf0849e48ce0b634e40f01ab
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-12 21:20:33 -0300

    components:Buttons/Button - Upgrading component

--- src/components/Buttons/Button/index.tsx
+++ src/components/Buttons/Button/index.tsx
@@ -1,17 +1,22 @@
-import { MouseEventHandler, ReactNode } from "react";
+import { CSSProperties, MouseEventHandler, ReactNode } from "react";
 import styles from "./button.module.css";
 
 type ButtonProps = {
     children: ReactNode;
-    type?: "default" | "light";
+    type?: "default" | "light-primary" | "light-dark";
+    rounded?: boolean;
     onClick?: MouseEventHandler<HTMLDivElement>;
+    style?: CSSProperties;
 };
 
-function Button({ children, type, onClick }: ButtonProps) {
+function Button({ children, type, onClick, style, rounded }: ButtonProps) {
     return (
         <div
             onClick={onClick}
-            className={styles.button + " " + styles[type ? type : "default"]}
+            style={style}
+            className={`${styles.button} ${styles[type ? type : "default"]} ${
+                rounded && styles.rounded
+            }`}
         >
             {children}
         </div>
